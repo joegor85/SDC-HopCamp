9125a7f15355e6fd15056d74780782394a7f8f15
root <joe.gor85@gmail.com>
2023-06-01 21:24:37 +0000

    modified all components to point to droplet ip address and server queries modified in comments

--- HopCamp/client/components/Tent_site/Tent_site.jsx
+++ HopCamp/client/components/Tent_site/Tent_site.jsx
@@ -54,7 +54,7 @@ const Tent_site = () => {
     };
 
     useEffect(()=>{
-        fetch('http://localhost:5001/api/campsites').then(responses=>responses.json())
+        fetch('http:159.223.122.220:5001/api/campsites').then(responses=>responses.json())
         .then(result=>{
             let rvArr= [];
             let lodgingArr = [];
